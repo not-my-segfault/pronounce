dc8a82c818fdca2f982b5b83896bcdd26b15ba67
michal <michal@tar.black>
2022-06-21 20:09:32 +0100

    changed url to github + formatting

--- app.py
+++ app.py
@@ -1,33 +1,42 @@
-from flask import Flask, render_template, request; from urllib.request import urlopen; import yaml
+from urllib.request import urlopen
+
+import yaml
+from flask import Flask, render_template, request
 
 app = Flask(
-        __name__,
-        template_folder='static')
+    __name__,
+    template_folder='static'
+)
 
 with open("conf.yml", "r") as global_conf_file:
     global_conf = yaml.load(global_conf_file, Loader=yaml.UnsafeLoader)
 
+
 @app.errorhandler(Exception)
 def basic_error(e):
     return render_template(
-            'error.html',
-            error=e,
-            url=request.url,
-            conf=global_conf,
-            color_bg=
-              global_conf['colors']['background'],
-            color_fg=
-              global_conf['colors']['foreground'],
-            color_h=
-              global_conf['colors']['heading'],
-            color_err=
-              global_conf['colors']['text'])
+        'error.html',
+        error=
+        e,
+        url=
+        request.url,
+        conf=
+        global_conf,
+        color_bg=
+        global_conf['colors']['background'],
+        color_fg=
+        global_conf['colors']['foreground'],
+        color_h=
+        global_conf['colors']['heading'],
+        color_err=
+        global_conf['colors']['text']
+    )
+
 
 @app.route("/robots.txt")
 def robots():
-    return """User-Agent: *
-              Disallow: /
-              """
+    return "User-Agent: * Disallow: /"
+
 
 @app.route("/<username>")
 def root(username):
@@ -40,30 +49,33 @@ def root(username):
     else:
         bg = conf['colors']['background']
     return render_template(
-            'index.html',
-            color_bg=
-              bg,
-            color_fg= 
-              conf.get("colors", {}).get("foreground", global_conf['colors']['foreground']),
-            color_name= 
-              conf.get("colors", {}).get("name", global_conf['colors']['heading']),
-            color_info= 
-              conf.get("colors", {}).get("info", global_conf['colors']['text']),
-            color_url= 
-              conf.get("colors", {}).get("url", {}).get("normal", global_conf['colors']['url']['normal']),
-            color_url_hover= 
-              conf.get("colors", {}).get("url", {}).get("hover", global_conf['colors']['url']['hover']),
-            title= 
-              conf.get("title", global_conf['title'].replace("{username}", f"{username}")),
-            icon= 
-              conf.get("icon", global_conf['icon']),
-            name= 
-              conf['name'],
-            age=
-              conf.get("age", ""),
-            pronouns= 
-    ", ".join(conf['pronouns']),
-            contacts= 
-              conf['contacts'],
-            urls= 
-              conf['urls'])
+        'index.html',
+        color_bg=
+        bg,
+        color_fg=
+        conf.get("colors", {}).get("foreground", global_conf['colors']['foreground']),
+        color_name=
+        conf.get("colors", {}).get("name", global_conf['colors']['heading']),
+        color_info=
+        conf.get("colors", {}).get("info", global_conf['colors']['text']),
+        color_url=
+        conf.get("colors", {}).get("url", {}).get("normal", global_conf['colors']['url']['normal']),
+        color_url_hover=
+        conf.get("colors", {}).get("url", {}).get("hover", global_conf['colors']['url']['hover']),
+        title=
+        conf.get("title", global_conf['title'].replace("{username}", f"{username}")),
+        icon=
+        conf.get("icon", global_conf['icon']),
+        name=
+        conf['name'],
+        age=
+        conf.get("age", ""),
+        pronouns=
+        ", ".join(
+            conf['pronouns']
+        ),
+        contacts=
+        conf['contacts'],
+        urls=
+        conf['urls']
+    )
